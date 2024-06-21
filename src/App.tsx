1eef528d82686409d731a4fea04b8d5dd9d4d969
GerardoAP22 <garriazapantaleon@colgate.edu>
2024-06-20 21:05:59 -0400

    button and alert exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import ListGroup from './components/ListGroup';
 import Alert from './components/Alert';
 import Button from './components/Button';
+import { useState } from 'react';
 
 function App() {
   let items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
   const handleSelectItem = (item: string) => {
     console.log(item);
   }
+  const [alertVisbile, setAlertVisibility] = useState(false);
 
   const handleTestButton = () => {
-    console.log('Clicked!');
+    setAlertVisibility(true);
   }
 
   return (
     <div>
       {/* <ListGroup items={items} heading='Cities' onSelectItem={handleSelectItem} /> */}
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
+      {alertVisbile && <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>}
       <Button onClick={handleTestButton}>
         My Button
       </Button>
